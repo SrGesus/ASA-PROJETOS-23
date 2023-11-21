ba2e0c9b09548f4dd1ac4680ab6391ceb1700686
SrGesus <108523575+SrGesus@users.noreply.github.com>
2023-11-21 22:34:44 +0000

    Mess

--- Proj1/src/main copy.rs
+++ Proj1/src/main copy.rs
@@ -29,56 +29,27 @@ struct Piece {
 }
 
 struct PriceTable {
-    // Optimal Piece Prices
     prices: Vec<Vec<Option<u32>>>,
-}
-
-struct Products {
-    // Client Piece Prices
-    products: Vec<Vec<u32>>,
-    sizes: Vec<usize>
-}
-
-impl Products {
-    pub fn new(x: usize, y: usize, n_pieces: usize) -> Self {
-        let max = x.max(y);
-        Self {
-            products: vec![vec![0; max]; max],
-            sizes: Vec::with_capacity(n_pieces)
-        }
-    }
-    pub fn insert(self: &mut Self, piece: Piece, price: u32) {
-        if piece.x-1 >= self.products.len() || piece.y-1 >= self.products.len() {
-            return;
-        }
-        self.products[piece.x-1][piece.y-1] = price;
-        self.products[piece.y-1][piece.x-1] = price;
-        self.sizes.push(piece.x);
-        self.sizes.push(piece.y);
-    }
+    original: Vec<usize>
 }
 
 impl PriceTable {
-    pub fn new(x: usize, y: usize) -> Self {
-        let max = x.max(y);
+    pub fn new(x: usize, y: usize, : usize) -> Self {
         Self { 
-            prices: vec![vec![None; max]; max],
+            prices: vec![vec![None; x.max(y)]; x.max(y)],
         }
     }
-
+    
     pub fn get(self: &Self, piece: &Piece) -> &Option<u32> {
         &self.prices[piece.x-1][piece.y-1]
     }
-
     pub fn insert(self: &mut Self, piece: Piece, price: u32) {
-        // assert!(self.prices.len() >= piece.x-1);
-        // assert!(self.prices[piece.x-1].len() >= piece.x);
         self.prices[piece.x-1][piece.y-1] = Some(price);
         self.prices[piece.y-1][piece.x-1] = Some(price);
     }
 }
 
-// static mut CALLS: i64 = -1;
+static mut CALLS: i64 = -1;
 
 impl Piece {
     pub fn new(x: usize, y: usize) -> Self {
@@ -90,28 +61,25 @@ impl Piece {
     pub fn cut_y(&self, dimension: usize) -> (Piece, Piece) {
         (Piece::new(self.x, self.y-dimension), Piece::new(self.x, dimension))
     }
-    pub fn get_price(self: Self, prices: &mut PriceTable, products: &Products) -> u32 {
+    pub fn get_price(self: Self, prices: &mut PriceTable) -> u32 {
+        unsafe {
+            CALLS += 1;
+        }
         if let Some(price) = prices.get(&self) { 
             return *price; 
         };
-        let mut price: u32 = products.products[self.x-1][self.y-1];
+        let mut price: u32 = 0;
         // Cut Horizontally
-        for i in &products.sizes {
-            if i >= &self.x {
-                break;
-            }
-            let (piece1, piece2) = Self::cut_x(&self, *i);
+        for i in 1..self.x/2+1 {
+            let (piece1, piece2) = Self::cut_x(&self, i);
             // println!("{:?} + {:?}", piece1, piece2);
-            price = price.max(piece1.get_price(prices, products) + piece2.get_price(prices, products));
+            price = price.max(Self::get_price(piece1, prices) + Self::get_price(piece2, prices));
         }
         // Cut Vertically
-        for i in &products.sizes {
-            if i >= &self.y {
-                break;
-            }
-            let (piece1, piece2) = Self::cut_y(&self, *i);
+        for i in 1..self.y/2+1 {
+            let (piece1, piece2) = Self::cut_y(&self, i);
             // println!("{:?} + {:?}", piece1, piece2);
-            price = price.max(piece1.get_price(prices, products) + piece2.get_price(prices, products));
+            price = price.max(Self::get_price(piece1, prices) + Self::get_price(piece2, prices));
         }
         prices.insert(self, price);
         price
@@ -121,21 +89,16 @@ impl Piece {
 fn main() {
     let (Some(x), Some(y)) = parse_line!(" ", usize, usize).unwrap() else { panic!()};
     // println!("{}, {}", x, y);
-    let (Some(n_pieces),) = parse_line!(" ", usize).unwrap() else { panic!()};
+    let (Some(_num_pieces),) = parse_line!(" ", usize).unwrap() else { panic!()};
 
     let mut prices: PriceTable = PriceTable::new(x,y);
 
-    let mut products: Products = Products::new(x, y, n_pieces);
-
     while let Ok((Some(a), Some(b), Some(p))) = parse_line!(" ", usize, usize, u32) {
         // println!("{} {} {}", a, b, p);
-        products.insert(Piece::new(a, b), p);
+        prices.insert(Piece::new(a, b), p);
     }
-    products.sizes.sort_unstable();
-    products.sizes.dedup();
-    println!("{:?}", products.sizes);
 
-    println!("{}", Piece::get_price(Piece::new(x, y), &mut prices, &products));
+    println!("{}", Piece::get_price(Piece::new(x, y), &mut prices));
     // Piece::get_price(Piece::new(x, y), &mut prices);
     // unsafe {
     //     println!("{},{},{}", x,y,CALLS);
